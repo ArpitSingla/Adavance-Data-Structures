f540c7727efdca9f9618efbf12f87d87143f302f
ArpitSingla <arpitsingla1999@gmail.com>
2019-10-10 14:42:31 +0530

    AVL Code for insertion

--- AVL.cpp
+++ AVL.cpp
@@ -3,22 +3,57 @@ using namespace std;
 
 struct node{
   int info;
-  node *left,*right;
+  node *left;
+  node *right;
   int height;
+};
+
+int height(node *n){
+  if(n==NULL){
+    return 0;
+  }
+  return n->height;
 }
 
-node *newNode(node *root, int info){
-  node *temp=new node();
-  temp->info=info;
-  temp->left=0;
-  temp->right=0;
-  temp->height=1;
+int balanceFactor(node *n){
+  if(n==NULL){
+    return 0;
+  }
+  return height(n->left)-height(n->right);
+}
+
+node *newNode(int info){
+	node *temp=new node();
+  	temp->info=info;
+  	temp->left=NULL;
+  	temp->right=NULL;
+  	temp->height=1;
+  	return temp;
+}
+
+node *leftRotate(node *root){
+  node *temp;
+  temp=root->right;
+  root->right=temp->left;
+  temp->left=root;
+  temp->height=1+max(height(temp->left),height(temp->right));
+  root->height=1+max(height(root->left),height(root->right));
+  return temp;
+}
+
+node *rightRotate(node *root){
+  node *temp;
+  temp=root->left;
+  root->left=temp->right;
+  temp->right=root;
+  temp->height=1+max(height(temp->left),height(temp->right));
+  root->height=1+max(height(root->left),height(root->right));
   return temp;
 }
 
 node *insertNode(node *root,int info){
   if(root==NULL){
-    return newNode(info);
+  	return newNode(info);
   }
   if(info<root->info){
     root->left=insertNode(root->left,info);
@@ -29,7 +64,7 @@ node *insertNode(node *root,int info){
   else{
     return root;
   }
-  node->height=1+max(height(root->left),height(root->right));
+  root->height=1+max(height(root->right),height(root->left));
   int balance=balanceFactor(root);
   if(balance>1 && info<root->left->info){
     return rightRotate(root);
@@ -48,72 +83,45 @@ node *insertNode(node *root,int info){
   return root;
 }
 
-node *leftRotate(node *root){
-  node *temp;
-  temp=root->right;
-  root->right=temp->left;
-  temp->left=root;
-  temp->height=1+max(height(temp->left),height(temp->right));
-  root->height=1+max(height(root->left),height(root->right));
-  return temp;
-}
-
-node *rightRotate(node *root){
-  node *temp;
-  temp=root->left;
-  root->left=temp->right`;
-  temp->right=root;
-  temp->height=1+max(height(temp->left),height(temp->right));
-  root->height=1+max(height(root->left),height(root->right));
-  return temp;
-}
-
-int balanceFactor(node *n){
-  if(n==NULL){
-    return 0;
-  }
-  return balanceFactor(root->left)-balanceFactor(root->right);
-}
-
-int height(node *n){
-  if(n==NULL){
-    return 0;
-  }
-  return n->height;
-}
-
 void inOrder(node *n){
-  inOrder(n->left);
-  cout<<n->info<<" ";
-  inOrder(n->right);
+	if(n!=NULL){
+		inOrder(n->left);
+  		cout<<n->info<<" ";
+  		inOrder(n->right);
+	}
 }
 void preOrder(node *n){
-  cout<<n->info<<" ";
-  inOrder(n->left);
-  inOrder(n->right);
+	if(n!=NULL){
+		cout<<n->info<<" ";
+		preOrder(n->left);
+  		preOrder(n->right);
+	}
 }
 void postOrder(node *n){
-  inOrder(n->left);
-  inOrder(n->right);
-  cout<<n->info<<" ";
+	if(n!=NULL){
+		postOrder(n->left);
+  		postOrder(n->right);
+  		cout<<n->info<<" ";
+	}
 }
 int main(){
-  int n,value;
-	cout<<"Enter the number of nodes: ";
-	cin>>n;
-	node *root;
-	cout<<"Enter the value of nodes: ";
-	cin>>value;
-	root=insertNode(root,value);
-	for(int i=0;i<n-1;i++){
-		cout<<"Enter the value of nodes: ";
-		cin>>value;
-		insert(root,value);
-	}
+	int n;
+    cin>>n;
+    node* root = NULL;
+    while(n--){
+        char c;
+        int a;
+        cin>>c>>a;
+        if (c == 'i'){
+            root = insertNode(root,a);
+        }
+//        else if (c == 'd'){
+//            root = deleteNode(root,a);
+//        }
+    }
   preOrder(root);
   cout<<endl;
   inOrder(root);
   cout<<endl;
   postOrder(root);
-  cout<<endl;
 }
